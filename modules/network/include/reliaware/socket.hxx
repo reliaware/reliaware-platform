a1e198e2dfd869a4783892d3fa01983c8f109b40
xerrial <dimass1129@gmail.com>
2025-11-18 14:43:20 +1000

    Add network/socket skeleton

---
+++ modules/network/include/reliaware/socket.hxx
@@ -0,0 +1,25 @@
+//
+// ReliaWare SDS platform
+// Copyright (c) 2025, ReliaWare
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#ifndef RELIAWARE_SOCKET_HXX
+#define RELIAWARE_SOCKET_HXX
+
+#pragma once
+
+namespace reliaware
+{
+    class socket
+    {
+    private:
+        int m_fd;
+
+    public:
+        socket(int domain, int type, int protocol);
+        virtual ~socket();
+    };
+};
+
+#endif // !RELIAWARE_SOCKET_HXX
---
+++ modules/network/src/socket.cxx
@@ -0,0 +1,26 @@
+//
+// ReliaWare SDS platform
+// Copyright (c) 2025, ReliaWare
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#include <reliaware/socket.hxx>
+
+#include <sys/socket.h>
+#include <unistd.h>
+#include <system_error>
+
+using namespace reliaware;
+
+socket::socket(int domain, int type, int protocol)
+    : m_fd(-1)
+{
+    if ((m_fd = ::socket(domain, type, protocol)) == -1)
+        throw std::system_error(errno, std::generic_category());
+}
+
+socket::~socket()
+{
+    if (m_fd != -1)
+        ::close(m_fd);
+}
