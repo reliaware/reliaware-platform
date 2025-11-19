30219b4c75af34bd1ab7de8239c9ed7acd93dc0d
Dmitry Sednev <dmitry@sednev.ru>
2025-11-19 08:19:50 +0300

    Add pidfile class

---
+++ modules/service/include/reliaware/pidfile.hxx
@@ -0,0 +1,19 @@
+//
+// ReliaWare platform
+// Copyright (c) 2025, ReliaWare
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#ifndef RELIAWARE_PIDFILE_HXX
+#define RELIAWARE_PIDFILE_HXX
+
+#pragma once
+
+namespace reliaware
+{
+    class pidfile
+    {
+    };
+};
+
+#endif // !RELIAWARE_PIDFILE_HXX
---
+++ modules/service/src/pidfile.cxx
@@ -0,0 +1,9 @@
+//
+// ReliaWare platform
+// Copyright (c) 2025, ReliaWare
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+#include <reliaware/pidfile.hxx>
+
+using namespace reliaware;
